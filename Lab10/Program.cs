6e691857bbc2fe7a293d0693017ea0bfdf4831e5
Heather <harperhe@hotmail.com>
2018-10-23 15:40:02 -0400

    inital commit

---
+++ Lab10/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Lab10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double UserInput = 0;
+            Circle myCircle = new Circle(UserInput);
+            Console.WriteLine("Please enter the radius of a circle!");
+            UserInput = double.Parse(Console.ReadLine());
+            myCircle.Radius = UserInput;
+            Console.WriteLine($"Circumference Formatted! {myCircle.CalculateFormattedCircumference()}");
+            Console.WriteLine($"Area: {myCircle.CalculateFormattedArea(),0:N2}");
+        }
+    }
+}
